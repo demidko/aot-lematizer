e66c60cb0012d0c8e2faa95909797e2a37d02cc6
Daniel Demidko <demidko@farpost.com>
2019-08-23 17:17:25 +1000

    Обновление повара

--- lematizer/src/main/java/com/farpost/aot/LuceneChiefCooker.java
+++ lematizer/src/main/java/com/farpost/aot/LuceneChiefCooker.java
@@ -6,16 +6,19 @@ import org.apache.lucene.util.IntsRef;
 import org.apache.lucene.util.IntsRefBuilder;
 import org.apache.lucene.util.fst.Util;
 
+/**
+ * Класс готовит строки на любой вкус
+ */
 class LuceneChiefCooker {
 
 	private static final IntsRefBuilder secretIngredientForLuceneString = new IntsRefBuilder();
 	private static final BytesRefBuilder secretIngredientForForJavaString = new BytesRefBuilder();
 
-	public static IntsRef prepareStringForLucene(String javaString) {
+	public static IntsRef prepareLuceneString(String javaString) {
 		return Util.toIntsRef(new BytesRef(javaString), secretIngredientForLuceneString);
 	}
 
-	public static String prepareStringForJava(IntsRef luceneString) {
+	public static String prepareJavaString(IntsRef luceneString) {
 		return Util.toBytesRef(luceneString, secretIngredientForForJavaString).utf8ToString();
 	}
 }
